7722b8b050b808dae09412283e160bd0c2c6c046
KKminwoo <ehdud6960@naver.com>
2021-11-14 23:01:21 +0900

    Update : open camera & video

--- ch02/camera_in.py
+++ ch02/camera_in.py
@@ -2,10 +2,12 @@ import sys
 import cv2
 
 
-# 카메라 열기
+# 카메라 여는 class 생성
+video = 'C:\coding\python\opencv\ch02\video1.mp4'
 cap = cv2.VideoCapture(0)
 
-if not cap.isOpened():
+# 정상적으로 카메라가 열었는지 확인
+if not cap.isOpened(): 
     print("Camera open failed!")
     sys.exit()
 
@@ -13,19 +15,29 @@ if not cap.isOpened():
 print('Frame width:', int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)))
 print('Frame height:', int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
 
+# 카메라 프레임 크기 조정
+# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+# cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+
 # 카메라 프레임 처리
 while True:
+    # 두 개의 변수 받아오는 순서 확인하기
     ret, frame = cap.read()
 
+    # retry 인자 확인
     if not ret:
         break
 
-    inversed = ~frame  # 반전
+    # 윤곽선만 딴 영상
+    edge = cv2.Canny(frame,50,150)
+    # 반전
+    inversed = ~frame 
 
     cv2.imshow('frame', frame)
     cv2.imshow('inversed', inversed)
+    cv2.imshow('edge', edge)
 
-    if cv2.waitKey(10) == 27:
+    if cv2.waitKey(10) == 27: # esc 눌렀을 때 빠져나옴
         break
 
 cap.release()
