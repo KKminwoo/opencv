94708839afded77f9541358a9c83e7daa82a80ab
KKminwoo <ehdud6960@naver.com>
2021-11-18 22:57:08 +0900

    Update : code Time check

--- ch02/time_check.py
+++ ch02/time_check.py
@@ -3,15 +3,17 @@ import time
 import numpy as np
 import cv2
 
-
+# 15MB 용량의 사진
 img = cv2.imread('hongkong.jpg')
 
+# 연산 시간을 측정
 tm = cv2.TickMeter()
 
 tm.reset()
 tm.start()
 t1 = time.time()
 
+# edge 검출해내는 함수
 edge = cv2.Canny(img, 50, 150)
 
 tm.stop()
