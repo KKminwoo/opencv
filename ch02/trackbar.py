6ef945d6556c01900fd0d5b11d53a37bfe50a196
KKminwoo <ehdud6960@naver.com>
2021-11-18 22:48:21 +0900

    Update : Study Track bar

--- ch02/trackbar.py
+++ ch02/trackbar.py
@@ -4,15 +4,20 @@ import cv2
 
 def on_level_change(pos):
     value = pos * 16
-    if value >= 255:
-        value = 255
+    # 포화 연산 : 255보다 값이 커질 경우 계속 255 값으로 나타내는 것 
+    value = np.clip(value,0,255)
+    # if value >= 255:
+    #     value = 255
 
     img[:] = value
     cv2.imshow('image', img)
 
-
+# grey scale 영상 출력
 img = np.zeros((480, 640), np.uint8)
+
 cv2.namedWindow('image')
+
+# 반드시 영상을 출력한 후 실행 0~16까지의 단계 실행
 cv2.createTrackbar('level', 'image', 0, 16, on_level_change)
 
 cv2.imshow('image', img)
