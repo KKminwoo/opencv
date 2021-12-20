640abf92a358306aed8c9d3b7a7bcf8d8eae1c66
KKminwoo <ehdud6960@naver.com>
2021-12-20 21:42:42 +0900

    Update : Study 평균값 필터

--- ch04/blurring1.py
+++ ch04/blurring1.py
@@ -11,7 +11,11 @@ if src is None:
 
 #kernel = np.ones((3, 3), dtype=np.float64) / 9.
 #dst = cv2.filter2D(src, -1, kernel)
-dst = cv2.blur(src, (3, 3))
+
+# 평균 값 필터링 함수
+# -> 영상의 특정 좌표 값을 주변 픽셀 값들의 산술 평균으로 설정
+# -> 픽셀들 간의 그레이스케일 값 변화가 줄어들어 날카로운 에지가 무뎌지고, 영상의 잡음의 영향이 사라짐
+dst = cv2.blur(src, (4, 4))
 
 cv2.imshow('src', src)
 cv2.imshow('dst', dst)
