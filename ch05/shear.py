5e7cf2a27d4f44a2025112036337b6c0d92b4c28
KKminwoo <ehdud6960@naver.com>
2021-12-21 21:07:09 +0900

    Update : Study shear tranform

--- ch05/shear.py
+++ ch05/shear.py
@@ -3,12 +3,13 @@ import numpy as np
 import cv2
 
 
-src = cv2.imread('tekapo.bmp')
+src = cv2.imread(r'C:\coding\python\opencv\ch05\tekapo.bmp')
 
 if src is None:
     print('Image load failed!')
     sys.exit()
 
+# 전단 변환 : x축과 y축 방향에 대해 따로 정의
 aff = np.array([[1, 0.5, 0],
                 [0, 1, 0]], dtype=np.float32)
 
--- ch05/translate.py
+++ ch05/translate.py
@@ -3,12 +3,13 @@ import numpy as np
 import cv2
 
 
-src = cv2.imread('tekapo.bmp')
+src = cv2.imread(r'C:\coding\python\opencv\ch05\tekapo.bmp')
 
 if src is None:
     print('Image load failed!')
     sys.exit()
 
+# 이동 변환 : x,y축 방향으로 이동 변위를 2*3 어파인 변환 행렬로 지정
 aff = np.array([[1, 0, 200],
                 [0, 1, 100]], dtype=np.float32)
 
