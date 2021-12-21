e4888387c06220cb633dc899402c24d815dca5c5
KKminwoo <ehdud6960@naver.com>
2021-12-21 21:07:00 +0900

    Update : Study scaling tranform

--- ch05/scaling.py
+++ ch05/scaling.py
@@ -3,12 +3,14 @@ import numpy as np
 import cv2
 
 
-src = cv2.imread('rose.bmp') # src.shape=(320, 480)
+src = cv2.imread(r'C:\coding\python\opencv\ch05\rose.bmp') # src.shape=(320, 480)
 
 if src is None:
     print('Image load failed!')
     sys.exit()
 
+# 크기 변환
+# x축과 y축 방향으로 스케일 비율을 지정
 dst1 = cv2.resize(src, (0, 0), fx=4, fy=4, interpolation=cv2.INTER_NEAREST)
 dst2 = cv2.resize(src, (1920, 1280))  # cv2.INTER_LINEAR
 dst3 = cv2.resize(src, (1920, 1280), interpolation=cv2.INTER_CUBIC)
