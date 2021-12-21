9c925ff949b3e4c0416e0edf6ab204e9936a9012
KKminwoo <ehdud6960@naver.com>
2021-12-21 21:06:36 +0900

    Update : Study rotation tranform

--- ch05/rotation1.py
+++ ch05/rotation1.py
@@ -4,13 +4,15 @@ import numpy as np
 import cv2
 
 
-src = cv2.imread('tekapo.bmp')
+src = cv2.imread(r'C:\coding\python\opencv\ch05\tekapo.bmp')
 
 if src is None:
     print('Image load failed!')
     sys.exit()
 
 rad = 20 * math.pi / 180
+
+# 영상을 특정 각도만큼 회전시키는 변환(반시계 방향)
 aff = np.array([[math.cos(rad), math.sin(rad), 0],
                 [-math.sin(rad), math.cos(rad), 0]], dtype=np.float32)
 
--- ch05/rotation2.py
+++ ch05/rotation2.py
@@ -3,12 +3,13 @@ import numpy as np
 import cv2
 
 
-src = cv2.imread('tekapo.bmp')
+src = cv2.imread(r'C:\coding\python\opencv\ch05\tekapo.bmp')
 
 if src is None:
     print('Image load failed!')
     sys.exit()
 
+# 영상의 중앙 기준 회전
 cp = (src.shape[1] / 2, src.shape[0] / 2)
 rot = cv2.getRotationMatrix2D(cp, 20, 0.5)
 
