4755df74bda457c1c35bd27d3d4328364cb1ad43
KKminwoo <ehdud6960@naver.com>
2021-12-22 21:37:47 +0900

    Update : Study perspective transform

--- ch05/perspective.py
+++ ch05/perspective.py
@@ -13,7 +13,9 @@ w, h = 720, 400
 srcQuad = np.array([[325, 307], [760, 369], [718, 611], [231, 515]], np.float32)
 dstQuad = np.array([[0, 0], [w-1, 0], [w-1, h-1], [0, h-1]], np.float32)
 
+# 투시변환 행렬
 pers = cv2.getPerspectiveTransform(srcQuad, dstQuad)
+# 영상의 투시변환 함수
 dst = cv2.warpPerspective(src, pers, (w, h))
 
 cv2.imshow('src', src)
